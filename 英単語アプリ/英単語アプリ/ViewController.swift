319fcb1b9fd63ab30854cf5668def0472775af30
Takumi Takamatu <e175744@ie.u-ryukyu.ac.jp>
2018-11-11 19:13:51 +0900

    画面遷移 タイトル-メニュー 訂正

--- 英単語アプリ/英単語アプリ/ViewController.swift
+++ 英単語アプリ/英単語アプリ/ViewController.swift
@@ -10,23 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func Start(_ sender: Any) {
-        
-    }
+ 
+    
+    @IBOutlet weak var button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // ボタンの装飾
         let rgba = UIColor(red: 255/255, green: 128/255, blue: 168/255, alpha: 1.0) // ボタン背景色設定
-        Start.backgroundColor = rgba                                               // 背景色
+        button.backgroundColor = rgba                                               // 背景色
         button.layer.borderWidth = 0.5                                              // 枠線の幅
         button.layer.borderColor = UIColor.black.cgColor                            // 枠線の色
         button.layer.cornerRadius = 5.0                                             // 角丸のサイズ
-        button.setTitleColor(UIColor.white, for: UIControlState.normal)
+        button.setTitleColor(UIColor.white, for: UIControl.State.normal)             // タイトルの色
     }
-
-
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
 
