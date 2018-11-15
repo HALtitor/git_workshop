5830192c3019e34a953c8c72528a14458a624921
Takumi Takamatu <e175744@ie.u-ryukyu.ac.jp>
2018-11-15 10:41:50 +0900

    画面遷移 色々

--- 英単語アプリ/英単語アプリ/ViewController.swift
+++ 英単語アプリ/英単語アプリ/ViewController.swift
@@ -15,16 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var button: UIButton!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        // ボタンの装飾
-        let rgba = UIColor(red: 255/255, green: 128/255, blue: 168/255, alpha: 1.0) // ボタン背景色設定
-        button.backgroundColor = rgba                                               // 背景色
-        button.layer.borderWidth = 0.5                                              // 枠線の幅
-        button.layer.borderColor = UIColor.black.cgColor                            // 枠線の色
-        button.layer.cornerRadius = 5.0                                             // 角丸のサイズ
-        button.setTitleColor(UIColor.white, for: UIControl.State.normal)             // タイトルの色
+
     }
     
     override func didReceiveMemoryWarning() {
