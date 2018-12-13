6af4a38932b4bf14bbd860f7f71da43e505383f7
Takumi Takamatu <e175744@ie.u-ryukyu.ac.jp>
2018-12-13 11:13:34 +0900

    score_result

--- EnglishStudyApp/EnglishStudyApp/ScoreViewController.swift
+++ EnglishStudyApp/EnglishStudyApp/ScoreViewController.swift
@@ -19,7 +19,46 @@ class ScoreViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let Qnum = appDelegate.Qindex
+        let correctnum = appDelegate.correctCount
         scoreLabel.text = appDelegate.Qindex.description + "問中" +  appDelegate.correctCount.description + "問正解！"
+        
+        /* --- 四角形を描画 --- */
+        let rectangleLayer = CAShapeLayer.init()
+        let rectangleFrame = CGRect.init(x: 20, y: 310, width: 800, height: 100)
+        rectangleLayer.frame = rectangleFrame
+        
+        // 輪郭の色
+        rectangleLayer.strokeColor = UIColor.blue.cgColor
+        // 四角形の中の色
+        rectangleLayer.fillColor = UIColor.blue.cgColor
+        // 輪郭の太さ
+        rectangleLayer.lineWidth = 2.5
+        
+        // 四角形を描画
+        rectangleLayer.path = UIBezierPath.init(rect: CGRect.init(x: 0, y: 0, width: rectangleFrame.size.width, height: rectangleFrame.size.height)).cgPath
+        
+        self.view.layer.addSublayer(rectangleLayer)
+        // Do any additional setup after loading the view.
+        
+        
+        /* --- 四角形を描画 --- */
+        let rectangleLayer2 = CAShapeLayer.init()
+        let rectangleFrame2 = CGRect.init(x: 20, y: 310, width: 8*correctnum*100 / Qnum, height: 100)
+        rectangleLayer2.frame = rectangleFrame2
+        
+        // 輪郭の色
+        rectangleLayer2.strokeColor = UIColor.red.cgColor
+        // 四角形の中の色
+        rectangleLayer2.fillColor = UIColor.red.cgColor
+        // 輪郭の太さ
+        rectangleLayer2.lineWidth = 2.5
+        
+        // 四角形を描画
+        rectangleLayer2.path = UIBezierPath.init(rect: CGRect.init(x: 0, y: 0, width: rectangleFrame2.size.width, height: rectangleFrame2.size.height)).cgPath
+        
+        self.view.layer.addSublayer(rectangleLayer2)
+
     }
     
 
