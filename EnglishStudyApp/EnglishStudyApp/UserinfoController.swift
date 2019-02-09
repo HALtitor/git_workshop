66eae2149e0ec371a84362c759136f58c2a6d1ac
e175751@ie.u-ryukyu.ac.jp <e175751 e175751@ie.u-ryukyu.ac.jp>
2019-02-09 20:48:29 +0900

    list change

--- EnglishStudyApp/EnglishStudyApp/UserinfoController.swift
+++ EnglishStudyApp/EnglishStudyApp/UserinfoController.swift
@@ -10,19 +10,20 @@ import UIKit
 
 class UserinfoController: UIViewController , UITableViewDelegate, UITableViewDataSource{
     
-    let TODO = ["牛乳を買う", "掃除をする", "アプリ開発の勉強をする"]
+    var DATE: [String] = []
+    
     // 遷移先のViewControllerに渡す変数
     var giveData: String = ""
 
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return TODO.count
+        return DATE.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let Cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
         
-         Cell.name.text = TODO[indexPath.item]
+         Cell.name.text = DATE[indexPath.item]
         return Cell
     }
     
@@ -36,13 +37,13 @@ class UserinfoController: UIViewController , UITableViewDelegate, UITableViewDat
     // 画面遷移の処理もここで書いている
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 押されたときのcellのlabelの文字列をViewControllerに渡したいので、一旦、giveDataに入れとく
-       giveData = TODO[indexPath.item]
-        // Segueを使った画面遷移を行う関数
-        performSegue(withIdentifier: "Segue", sender: nil)
+        
+            giveData = DATE[indexPath.item]
+            // Segueを使った画面遷移を行う関数
+            performSegue(withIdentifier: "Segue", sender: nil)
+        
     }
 
-    
-    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
         if segue.identifier == "Segue" {
